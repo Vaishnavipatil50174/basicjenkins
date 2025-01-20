a403c66d67be786a4ef48133c3b471a5bd35bca4
vaishnavi <vaishnaviravindra_patil@epam.com>
2025-01-20 12:29:26 +0530

    forcefully compilation error

--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class JenkinsApplicationTests {
 	private static final Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing");
 		logger.info("Test case executing second log statement");
 		Assertions.assertEquals(true, true);
