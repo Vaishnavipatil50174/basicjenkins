534d2ccd619eaf9048348f39681ab623b65f87c4
vaishnavi <vaishnaviravindra_patil@epam.com>
2025-01-20 12:14:53 +0530

    some changes

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -1,20 +1,19 @@
 package com.example.jenkins;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 public class JenkinsApplication {
 
-
-	public  static Logger logger= (Logger) LoggerFactory.getLogger(JenkinsApplication.class);
+	// Correct way to declare SLF4J logger
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
 
 	@PostConstruct
-	public void init(){
+	public void init() {
 		logger.info("Application Started");
 	}
 
@@ -23,5 +22,4 @@ public class JenkinsApplication {
 
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
-
 }
--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -2,21 +2,20 @@ package com.example.jenkins;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.logging.Logger;
-
 @SpringBootTest
-
 class JenkinsApplicationTests {
 
-	public  static Logger logger= (Logger) LoggerFactory.getLogger(JenkinsApplication.class);
+	// Correct logger type
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
-		logger.info("test case executing");
-		Assertions.assertEquals(true,true);
+		logger.info("Test case executing");
+		Assertions.assertEquals(true, true);
 	}
-
 }
+
